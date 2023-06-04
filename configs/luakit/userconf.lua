fe60e6c9f3cc68cc38c4c6a9a45b179e30e50ab0
idm1try <dimasavchenko1706@icloud.com>
2023-06-05 00:26:41 +0500

    chore(luakit): add more search engines

--- configs/luakit/userconf.lua
+++ configs/luakit/userconf.lua
@@ -10,7 +10,18 @@ select.label_maker = function(s)
 end
 follow.pattern_maker = follow.pattern_styles.match_label
 
-settings.window.default_search_engine = "duckduckgo"
+settings.window.search_engines = {
+	searxng = "https://xo.wtf/search?q=%s",
+	ddg = "https://duckduckgo.com/?q=%s",
+	github = "https://github.com/search?q=%s",
+	wiki = "https://en.wikipedia.org/wiki/Special:Search?search=%s",
+	invidious = "https://invidious.snopyta.org/search?q=%s",
+	reddit = "https://reddi.tk/search?q=%s",
+	nix = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=%s",
+	osm = "https://www.openstreetmap.org/search?query=%s",
+}
+
+settings.window.default_search_engine = "searxng"
 settings.webview.enable_webaudio = true
 settings.window.home_page = "luakit://newtab/"
 settings.webview.monospace_font_family = "CartographCF Nerd Font"
