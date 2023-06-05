b86092ab5bee95a6d1987692d5cfac43e551e877
idm1try <dimasavchenko1706@icloud.com>
2023-06-05 13:55:14 +0500

    feat: custom hosts file

--- hosts/yuki/default.nix
+++ hosts/yuki/default.nix
@@ -55,6 +55,19 @@
 
   boot.loader.systemd-boot.enable = true;
   networking.networkmanager.enable = true;
+  # block proprietary shit
+  networking.extraHosts = ''
+    0.0.0.0 youtube.com
+    0.0.0.0 www.youtube.com
+    0.0.0.0 reddit.com
+    0.0.0.0 www.reddit.com
+    0.0.0.0 ufanet.ru
+    0.0.0.0 www.ufanet.ru
+    0.0.0.0 mail.ru
+    0.0.0.0 www.mail.ru
+    0.0.0.0 twitter.com
+    0.0.0.0 www.twitter.com
+  '';
   systemd.services.NetworkManager-wait-online.enable = false;
 
   services = {
