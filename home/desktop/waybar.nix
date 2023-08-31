c40f1730fc1cd0ca3456f32ed728a328617b4c31
idm1try <dimasavchenko1706@icloud.com>
2023-09-01 00:23:19 +0500

    chore(waybar): remove some components

--- home/desktop/waybar.nix
+++ home/desktop/waybar.nix
@@ -9,7 +9,7 @@
       height = 64;
       modules-left = [ "sway/workspaces" ];
       modules-center = [ ];
-      modules-right = [ "sway/language" "memory" "cpu" "battery" "clock" ];
+      modules-right = [ "sway/language" "battery" "clock" ];
       "sway/workspaces" = {
         disable-scroll = true;
         disable-click = true;
@@ -24,18 +24,6 @@
           "<span font_desc='CartographCF Nerd Font Heavy Italic' color='#${colors.mauve}'>bat</span> {capacity}%";
         format-charging =
           "<span font_desc='CartographCF Nerd Font Heavy Italic' color='#${colors.mauve}'>bat [ac]</span> {capacity}%";
-        interval = 5;
-        tooltip = false;
-      };
-      "cpu" = {
-        format =
-          "<span font_desc='CartographCF Nerd Font Heavy Italic' color='#${colors.mauve}'>cpu</span> {usage}%";
-        interval = 5;
-        tooltip = false;
-      };
-      "memory" = {
-        format =
-          "<span font_desc='CartographCF Nerd Font Heavy Italic' color='#${colors.mauve}'>ram</span> {used}gb";
         interval = 5;
         tooltip = false;
       };
