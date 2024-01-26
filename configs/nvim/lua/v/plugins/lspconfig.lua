b669e004f71f04494d27fa3b0a3974124c9c7ced
idm1try <dimasavchenko1706@icloud.com>
2024-01-27 01:07:49 +0500

    cleanup capabilities

--- configs/nvim/lua/v/plugins/lspconfig.lua
+++ configs/nvim/lua/v/plugins/lspconfig.lua
@@ -106,12 +106,7 @@ return {
       end,
     })
 
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      require("cmp_nvim_lsp").default_capabilities()
-    )
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     local lspconfig = require("lspconfig")
 
