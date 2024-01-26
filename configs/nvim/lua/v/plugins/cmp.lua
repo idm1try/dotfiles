cf0793e335982a758822aa05913b54b84e8d8334
idm1try <dimasavchenko1706@icloud.com>
2024-01-27 00:11:08 +0500

    update

--- configs/nvim/lua/v/plugins/cmp.lua
+++ configs/nvim/lua/v/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    version = false,
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
@@ -8,9 +9,12 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
     },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
       local kinds = {
         Text = "",
@@ -75,18 +79,40 @@ return {
             hl_group = "CmpGhostText",
           },
         },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
         mapping = {
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         },
         sources = {
           { name = "path" },
           { name = "nvim_lsp" },
+          { name = "luasnip" },
           { name = "nvim_lua" },
           { name = "buffer" },
         },
