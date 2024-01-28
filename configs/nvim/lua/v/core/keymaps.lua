6b3500246598ff471dec648ab82940287cf5d4e2
idm1try <dimasavchenko1706@icloud.com>
2024-01-28 11:36:05 +0500

    chore(nvim): hop fork; cowboy

--- configs/nvim/lua/v/core/keymaps.lua
+++ configs/nvim/lua/v/core/keymaps.lua
@@ -1,4 +1,7 @@
 local map = vim.keymap.set
+local utils = require("v.core.utils")
+
+utils.cowboy()
 
 map("n", "<leader>q", "<CMD>bd<CR>", { desc = "[buffer] close current" })
 map("n", "<leader>Q", "<CMD>%bd|e#|bd#<CR>", { desc = "[buffer] close other" })
