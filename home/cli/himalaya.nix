20eb5709fa585f7a6e127fd858200abd0eb8815b
idm1try <dimasavchenko1706@icloud.com>
2024-03-09 15:05:38 +0500

    fix: himalaya config

--- home/cli/himalaya.nix
+++ home/cli/himalaya.nix
@@ -1,24 +1,21 @@
 { ... }: {
-  programs.himalaya = {
-    enable = true;
-    settings.accounts.idm1try = {
-      backend = "imap";
-      email = "dimasavchenko1706@icloud.com";
-      default = true;
-      display-name = "idm1try";
-      imap.host = "imap.mail.me.com";
-      imap.port = 993;
-      imap.encryption = true;
-      imap.login = "dimasavchenko1706@icloud.com";
-      imap.auth = "passwd";
-      imap.passwd.cmd = "cat /var/secrets/mail";
-      message.send.backend = "smtp";
-      smtp.host = "smtp.mail.me.com";
-      smtp.port = 587;
-      smtp.encryption = "start-tls";
-      smtp.login = "dimasavchenko1706@icloud.com";
-      smtp.auth = "passwd";
-      smtp.passwd.cmd = "cat /var/secrets/mail";
+  accounts.email.accounts.idm1try = {
+    primary = true;
+    userName = "idm1try";
+    realName = "Dmitry Savchenko";
+    address = "dimasavchenko1706@icloud.com";
+    aliases = [ "idm1try@icloud.com" ];
+    passwordCommand = "cat /var/secrets/mail";
+    imap = {
+      host = "imap.mail.me.com";
+      port = 993;
     };
+    smtp = {
+      host = "smtp.mail.me.com";
+      port = 587;
+      tls.useStartTls = true;
+    };
+    himalaya.enable = true;
   };
+  programs.himalaya.enable = true;
 }
