ee4ccf3d9102fa05e56db3cda4ecb2cd0dcf0766
idm1try <dimasavchenko1706@icloud.com>
2024-04-05 21:10:23 +0500

    update gitconfig

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -4,6 +4,7 @@
     userName = "idm1try";
     userEmail = "dimasavchenko1706@icloud.com";
     extraConfig = {
+      github.user = "idm1try";
       init.defaultBranch = "main";
       push.default = "current";
     };
